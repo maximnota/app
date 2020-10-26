086a183d4ae38e6cb3c87c66fe16d055a4a97f3e
Maxim Gannota <maximnota@icloud.com>
2020-10-26 21:52:39 +0200

    my first commit

---
+++ app.py
@@ -0,0 +1,116 @@
+def poopie_1():
+	while (True):
+		print("enter login:")
+		login = input() 
+		print("enter password:")        
+		password = input()
+
+		if (login=="login") and (password=="password"):
+			print("success :=@")
+			break
+
+		print("please enter all again hahahahahahahahahahahahaha!")
+
+def poopie_2():
+	while (True):
+		print("enter login:")
+		login = input() 
+		print("enter password:")        
+		password = input()
+
+		if (login=="login") or (password=="password"):
+			print("success ???aaaa")
+			break
+
+		print("please enter all again hahahahahahahahahahahahaha!")
+
+poopie_1()
+poopie_2()
+
+
+
+
+
+# import os
+	
+
+
+# for i in range(3):
+# 	print("hello" )
+
+
+# def print_names():
+# 	print("----------------------------------------------")
+# 	print("Hello!")
+# 	print("1. My name is Max. What is your name?")
+# 	print("2. My name is Max. \tWhat is your name?")
+# 	print("3. My name is Max. \nWhat is your name?")
+# 	print("----------------------------------------------")
+
+# 	os.system("say \"Kostya\"")
+
+# while (True):
+# 	print("Enter the password:")
+# 	password = input()
+
+# 	if (password == ""):
+# 		break
+
+# 	os.system("say \"You're stupid!\"")
+
+
+# login: 	ivan
+# password:	pass
+
+
+ 
+# print("what is your name?")
+# name = input()
+# print("whats your surname?")
+# surname = input() 
+# print("whats your age?")
+# age = input()
+# print("what is your company")
+# company =input()
+# print("whats your mothers name?")
+# mum_name = input()
+# print("why do you want to work at " + company + "?")
+# why = input()
+# print("what symbol does have " + company + "?")
+# symbol= input()
+# print("your name is " + name)
+# print("your surname is " + surname)
+# print("your age is " + age)
+# print("your company is " + company )
+# print("your mothers name is " + mum_name)
+# print("you want to work at "+ company +"  bacuase " + why)
+# print("the symbol is " + symbol)
+
+
+
+# 1. What's you name?
+# 2. What's your surname?
+# 3. What's you age?
+# 4. Your full name is .... You're ... year(s) old.
+
+
+
+# print("my name is max what is your name" 
+# 	"\n\tmy name is max what is your name?" 
+# 	"\nmyname is max \nwhat is your name?")
+
+# print("")
+# print("Hello, my name is Daniil Gannota")
+# print("Hello, " 
+# 	"my name is Daniil Gannota")
+
+# print("Hello, " 
+# 	"\nmy name is Daniil Gannota")
+
+
+
+# print("Hello!"
+# 	"\n1. My name is Max. What is your name?"
+# 	"\n2. My name is Max.\tWhat is your name?"
+# 	"\n3. My name is Max.\nWhat is your name?") 
+# print("----------------------------------------------")
